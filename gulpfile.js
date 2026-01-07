04647b931040b12b8f559af967679529d1d8d3e4
Dima121111 <157640077+Dima121111@users.noreply.github.com>
2026-01-07 14:32:49 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const { src, dest, watch, parallel } = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+const fileinclude = require('gulp-file-include');
+
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(concat('style.min.css'))
+        .pipe(scss({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 versions'],
+            grid: true
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream());
+}
+
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream());
+}
+
+function htmlInclude() {
+    return src('app/html/pages/**/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(dest('app'))
+        .pipe(browserSync.stream());
+}
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: 'app/'
+        },
+        notify: false
+    })
+}
+
+function watching() {
+    watch(['app/scss/**/*.scss'], styles);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch('app/html/**/*.html', htmlInclude);
+}
+
+exports.styles = styles
+exports.scripts = scripts
+exports.watching = watching
+exports.browsersync = browsersync
+exports.htmlInclude = htmlInclude
+exports.default = parallel(styles, scripts, watching, browsersync, htmlInclude)
